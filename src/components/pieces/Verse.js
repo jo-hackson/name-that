a7ccc3311eac94e293dd441c97ff9ff2bbf594e7
Joanna <joanna.yeh.jackson@gmail.com>
2018-02-23 11:13:06 -0600

    Add Verse back with capitalization

---
+++ src/components/pieces/Verse.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AnswerFeedback from './AnswerFeedback';
+
+class Verse extends React.Component {
+
+	render() {
+
+		return(
+			<div>
+				<h1><span id="quotes">&#8220;</span>{this.props.verse.verse}<span id="quotes">&#8221;</span></h1>
+				<AnswerFeedback 
+					correctAnswer={this.props.verse.book} 
+					updateScore={this.props.updateScore} 
+				/>
+			</div>
+		);
+	}
+}
+
+
+Verse.propTypes = {
+	verse: PropTypes.shape({
+		verse: PropTypes.string.isRequired,
+		book: PropTypes.string.isRequired
+	}).isRequired,
+	updateScore: PropTypes.func.isRequired
+};
+
+
+export default Verse;
