9e626db4954532fca5d0a9b97135d3ae5f41dcfd
Joanna <joanna.yeh.jackson@gmail.com>
2018-02-23 16:13:28 -0600

    Successful API call to Bible API to grab verses, able to grab book and verse and set state with that information

--- src/components/pages/GamePage.js
+++ src/components/pages/GamePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import './../../styles/GamePage.css';
-import Copyright from '../pieces/Copyright';
+// import Copyright from '../pieces/Copyright';
 import Verse from '../pieces/Verse';
 import Timer from '../pieces/Timer';
 
@@ -22,30 +22,82 @@ class GamePage extends React.Component {
 	componentWillMount() {
 		this.getVerses();
 	};
+
+
+	whichVerse = verseResponse => {
+			// remove leading numbers in verse
+			return verseResponse.replace(/^\d+\s*/, '');
+	};
+	
+	whichBook = bookResponse => {
+		// bookResponse = 1 John 3:16 (ASV 1901)
+		let book = "";
+	// if first character is a number then need to take it out and then do the normal expression
+  	if (bookResponse.match(/\D/) != null) {
+  		book += bookResponse[0]; // add the number to the book
+  		bookResponse = bookResponse.slice(1); // remove number
+  	}
+
+  	// extract the name
+  	book += bookResponse.match(/^\D+/)[0]
+  	return book.trim();
+	};
+
+	
+	shuffle = array => {
+		console.log("in shuffle");
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+		return array;
+	};
 		
 	// setState in here with result
 	getVerses = () => {
 		console.log("getting verses...");
 
-		// axios.get("https://api.esv.org/v3/passage/html/?q=Jn11.35", { 
-		// 	headers: { 
-		// 		Authorization: `Token ${process.env.REACT_APP_BIBLE_API_KEY}`, 
-		// 	}})
-		//   .then(response => {
-		//   	console.log(response)
-		//   	console.log("success")
-		//   })
-		//   .catch(errors => {
-		//   	console.log("fail")
-		//   	console.log(errors)
-		//   });
+		// do 5 API calls	
+		var verseArray = ["Romans8:1", "Romans8:2", "Romans8:3", "Romans8:4", "Romans8:5", "Romans8:6"];
 
+		var versesToApi = this.shuffle(verseArray);
+		var verseObjectArray = [];
 
+		for (var i = 0; i < versesToApi.length - 1; i++) {
 
-		this.setState({ verseList: [{"verse": "in the beginning was God", "book": "genesis"}, 
-																{"verse": "second verse", "book": "exodus"},
-																{"verse": "third verse", "book": "numbers"}] 
-								 })									
+			axios.get(`https://api.biblia.com/v1/bible/content/ASV.txt.js?passage=${versesToApi[i]}&style=fullyFormatted&key=${process.env.REACT_APP_BIBLIA_API_KEY}`)
+					  .then(response => {
+					  	// console.log(response.data.text);
+					  	let bookResponse = response.data.text.split('\n')[0];
+					  	if (response.data.text.split('\n')[1].match(/\r/) != null) {
+					  		var verseResponse = response.data.text.split('\n')[2]
+					  	} else {
+					  		var verseResponse = response.data.text.split('\n')[1];
+					  	}
+					  	// console.log("verse response is " + verseResponse);
+
+					  	// find book
+					  	let book = this.whichBook(bookResponse);
+					  	// console.log("final book is " + book);
+
+					  	// find verse
+					  	let verse = this.whichVerse(verseResponse);
+					  	// console.log("final verse is " + verse);
+					   
+					  	verseObjectArray.push({ "verse": verse, "book": book });
+					  	this.setState({ verseList: verseObjectArray });
+					  })
+					  .catch(errors => {
+					  	console.log("fail")
+					  	console.log(errors)
+					  });
+		}							
 	};
 
 
@@ -80,7 +132,7 @@ class GamePage extends React.Component {
 				)}
 
 
-				<Copyright />
+				{/* <Copyright /> */}
 			</div>
 		);
 	}
